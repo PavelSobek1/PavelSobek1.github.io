c95deb82672e756da8bf1e8fdb78c595c6e3db03
Pavel <textar.cz@gmail.com>
2018-10-28 12:28:46 +0100

    data

--- blog.js
+++ blog.js
@@ -6,3 +6,8 @@ $(`button`).on(`click`, (event) => {
     <div class="content">${content}</div>
     </div>`);
 })
+let path = "posts/1";
+let dataToSave = {
+  title: "My first saved blog post",
+  text: "Some hilarious content, which proves how awesome I am."
+};
